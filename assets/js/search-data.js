967f137ca39c23c554bbe643d4878d885dc21491
bdearaujo <bdearaujo@gmail.com>
2025-06-10 02:17:06 +0000

    Deploying to gh-pages from @ bdearaujo/bdearaujo.github.io@29b01cadadaaab0a200c7769c74b407bffa4b231 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,9 +45,9 @@ ninja.data = [{
           description: "A Low-Cost, High Precision, High Accuracy Tool to Measure the Latency of Touchscreen Devices",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-sensing-hand-interactions",
+          title: 'Sensing Hand Interactions',
+          description: "with Everyday Objects by Profiling Wrist Topography",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-mockup-builder",
@@ -55,9 +55,9 @@ ninja.data = [{
           description: "Direct 3D Modeling On and Above the Surface in a Continuous Interaction Space",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-next-point-prediction-metrics",
+          title: 'Next-Point Prediction Metrics',
+          description: "for Perceived Spatial Errors",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-storeoboard",
